000951c93abdd4b233f4ebc4f5652d326de4070d
Matt Williams <matt_williams_5@hotmail.com>
2017-01-09 17:49:50 +0000

    First Chef Update

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,10 @@
+default["apache"]["sites"]["matt-williams-52"] = { "site_title" => "Matt-Williams-52 websites coming soon", "port" => 80, "domain" => "matt-williams-52.mylabserver.com" }
+default["apache"]["sites"]["matt-williams-52b"] = { "site_title" => "Matt-Williams-52b websites coming soon!", "port" => 80, "domain" => "matt-williams-52b.mylabserver.com" }
+default["apache"]["sites"]["matt-williams-53"] = { "site_title" => "Matt-Williams-53 websites coming soon!", "port" => 80, "domain" => "matt-williams-53.mylabserver.com" }
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
